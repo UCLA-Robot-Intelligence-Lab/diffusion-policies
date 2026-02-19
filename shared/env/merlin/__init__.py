f78c29e921b4e0d5101335787d64fe0ee5df991e
raayandhar <raayan.dhar@gmail.com>
2026-02-19 13:58:33 -0800

    inference code
    Signed-off-by: raayandhar <raayan.dhar@gmail.com>

--- shared/env/merlin/__init__.py
+++ shared/env/merlin/__init__.py
@@ -1,3 +1,4 @@
 from .merlin_image_dataset import MerlinImageDataset
+from .merlin_inference import MerlinPolicyInference
 
-__all__ = ["MerlinImageDataset"]
+__all__ = ["MerlinImageDataset", "MerlinPolicyInference"]
